c0f459909f248842d2e8950a9bc14cdd05c4cc66
Adam <77313892+Halcyon28@users.noreply.github.com>
2026-07-12 09:32:20 -0700

    fixed totally fr

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        apod: resolve(__dirname, 'apod.html'),
+        portfolio: resolve(__dirname, 'portfolio.html'),
+        chickens: resolve(__dirname, 'Chickens.html'),
+      },
+    },
+  },
 })
